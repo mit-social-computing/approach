ffc9f17c4671ac8659460a24e8922c3e86001de2
Brian Whitton <me@brianwhitton.com>
2013-11-19 14:41:55 -0500

    import db script ready for use

---
+++ .bak/import_db.php
@@ -0,0 +1,41 @@
+<?php
+
+/* 
+  Title: import_db.php
+  Purpose - Import database into an ExpressionEngine install
+  Author - Doug Avery <doug.avery@viget.com>
+*/
+
+/* @group get DB info */
+
+	define('BASEPATH', true);
+	include '../ee/expressionengine/config/database.php';
+
+/* @end */
+
+/* @group drop, create, import DB */
+
+	exec("mysql \
+		--user={$db[$env]['username']} \
+		--password={$db[$env]['password']} \
+		-e 'drop database {$db[$env]['database']}'");
+
+	exec("mysql \
+		--user={$db[$env]['username']} \
+		--password={$db[$env]['password']} \
+		-e 'create database {$db[$env]['database']}'");
+	
+	exec("mysql \
+		--user={$db[$env]['username']} \
+		--password={$db[$env]['password']} \
+		{$db[$env]['database']} < sql/db_dump.sql");
+
+/* @end */
+
+/* @group feeback */
+
+	echo("Import database: {$db[$env]['database']}\n");
+
+/* @end */
+
+?>
