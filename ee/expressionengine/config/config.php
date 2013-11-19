3df596bc47c7a6386b3fed1963277df56bdac540
Brian Whitton <me@brianwhitton.com>
2013-11-19 15:16:17 -0500

    moved db definitions to top of cfg, updated with Low variable settings and added low variable folders

--- ee/expressionengine/config/config.php
+++ ee/expressionengine/config/config.php
@@ -1,92 +1,5 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-if( isset( $_SERVER['argc'] ) ) {
-
-    $env = strpos( $_SERVER['PWD'], 'staging') ? 'staging' : 'production';
-    $env = strpos( $_SERVER['PWD'], 'www') ? $env : 'local';
-
-} else {
-
-    /*
-     * Dynamic Configs
-     --------------------------------------------------------------------------------*/
-
-    $s = 'BASE INSTALL';
-
-    $base_url = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https' : 'http');
-    $base_url .= '://'.$_SERVER['HTTP_HOST'].'/';
-    $admin_url  = $base_url . 'cms.php';
-
-    $env = strpos($base_url, 'staging') ? 'staging' : 'production';
-    $env = strpos($base_url, 'dev') ? 'local' : $env;
-
-    $config['app_version'] = '272';
-    $config['install_lock'] = '';
-    $config['license_number'] = "CORE";
-    $config['debug'] = '1';
-    $config['doc_url'] = "http://ellislab.com/expressionengine/user-guide/";
-    $config['is_system_on'] = "y";
-    $config['allow_extensions'] = 'y';
-    $config['cookie_prefix'] = '';
-    $config['save_tmpl_files'] = 'y';
-
-    $config['site_url'] = $base_url;
-    $config['cp_url'] = $admin_url;
-    $config['webroot'] = FCPATH;
-    $config['system_path'] = $config['webroot'] . '../ee';
-    $config['site_label'] = 'Base Install';
-
-    $config['theme_folder_url'] = $config['site_url'].'themes/';
-    $config['theme_folder_path'] = $config['webroot'].'themes/';
-
-    $config['tmpl_file_basepath'] = $config['webroot'].'assets/templates/';
-
-    $config['avatar_url'] = $base_url.'images/avatars/';
-    $config['avatar_path'] = $config['webroot'].'images/avatars/';
-    $config['photo_url'] = $base_url.'images/member_photos/';
-    $config['photo_path'] = $config['webroot'].'images/member_photos/';
-    $config['sig_img_url'] = $base_url.'images/signature_attachments/';
-    $config['sig_img_path'] = $config['webroot'].'images/signature_attachments/';
-    $config['prv_msg_upload_path'] = $config['webroot'].'images/pm_attachments/';
-
-    // END EE config items
-
-    /* 
-     * CodeIgniter Configuration
-     ------------------------------------------------------------------*/
-
-    $config['base_url'] = $config['site_url'];
-    $config['index_page'] = "";
-    $config['uri_protocol']	= 'AUTO';
-    $config['url_suffix'] = '';
-    $config['language']	= 'english';
-    $config['charset'] = 'UTF-8';
-    $config['enable_hooks'] = FALSE;
-    $config['subclass_prefix'] = 'EE_';
-    $config['permitted_uri_chars'] = 'a-z 0-9~%.:_\\-';
-    $config['enable_query_strings'] = FALSE;
-    $config['directory_trigger'] = 'D';
-    $config['controller_trigger'] = 'C';
-    $config['function_trigger'] = 'M';
-    $config['log_threshold'] = 0;
-    $config['log_path'] = '';
-    $config['log_date_format'] = 'Y-m-d H:i:s';
-    $config['cache_path'] = '';
-    $config['encryption_key'] = '';
-    $config['global_xss_filtering'] = FALSE;
-    $config['csrf_protection'] = FALSE;
-    $config['compress_output'] = FALSE;
-    $config['time_reference'] = 'local';
-    $config['rewrite_short_tags'] = TRUE;
-    $config['proxy_ips'] = '';
-}
-
-/* 
- * Universal database connection settings
- -------------------------------------------------------------------*/
-$active_group = $env;
-$active_record = TRUE;
-
 $db['local']['hostname'] = '127.0.0.1';
 $db['local']['username'] = 'root';   
 $db['local']['password'] = 'root'; 
@@ -99,6 +12,100 @@ $db['production']['hostname'] = 'PRODUCTIONHOST';
 $db['production']['username'] = 'PRODUCTIONUSER';
 $db['production']['password'] = 'PRODUCTIONPASS';
 $db['production']['database'] = 'PRODUCTIONDB';
+
+if( isset( $_SERVER['argc'] ) ) {
+
+    $env = strpos( $_SERVER['PWD'], 'staging') ? 'staging' : 'production';
+    $env = strpos( $_SERVER['PWD'], 'www') ? $env : 'local';
+
+    return;
+}
+
+/*
+ * Dynamic Configs
+ --------------------------------------------------------------------------------*/
+
+$s = 'BASE INSTALL';
+
+$base_url = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https' : 'http');
+$base_url .= '://'.$_SERVER['HTTP_HOST'].'/';
+$admin_url  = $base_url . 'cms.php';
+
+$env = strpos($base_url, 'staging') ? 'staging' : 'production';
+$env = strpos($base_url, 'dev') ? 'local' : $env;
+
+$config['app_version'] = '272';
+$config['install_lock'] = '';
+$config['license_number'] = "CORE";
+$config['debug'] = '1';
+$config['doc_url'] = "http://ellislab.com/expressionengine/user-guide/";
+$config['is_system_on'] = "y";
+$config['allow_extensions'] = 'y';
+$config['cookie_prefix'] = '';
+$config['save_tmpl_files'] = 'y';
+
+$config['site_url'] = $base_url;
+$config['cp_url'] = $admin_url;
+$config['webroot'] = FCPATH;
+$config['system_path'] = $config['webroot'] . '../ee';
+$config['site_label'] = 'Base Install';
+
+$config['theme_folder_url'] = $config['site_url'].'themes/';
+$config['theme_folder_path'] = $config['webroot'].'themes/';
+
+$config['tmpl_file_basepath'] = $config['webroot'].'assets/templates/';
+
+$config['avatar_url'] = $base_url.'images/avatars/';
+$config['avatar_path'] = $config['webroot'].'images/avatars/';
+$config['photo_url'] = $base_url.'images/member_photos/';
+$config['photo_path'] = $config['webroot'].'images/member_photos/';
+$config['sig_img_url'] = $base_url.'images/signature_attachments/';
+$config['sig_img_path'] = $config['webroot'].'images/signature_attachments/';
+$config['prv_msg_upload_path'] = $config['webroot'].'images/pm_attachments/';
+
+/*
+* Low Variables Config
+* -------------------------------------------------------------------------------*/
+$config['low_variables_license_key'] = '';
+$config['low_variables_save_as_files'] = 'y';
+$config['low_variables_file_path'] = $config['tmpl_file_basepath'].'low/';
+
+// END EE config items
+
+/* 
+ * CodeIgniter Configuration
+ ------------------------------------------------------------------*/
+
+$config['base_url'] = $config['site_url'];
+$config['index_page'] = "";
+$config['uri_protocol']	= 'AUTO';
+$config['url_suffix'] = '';
+$config['language']	= 'english';
+$config['charset'] = 'UTF-8';
+$config['enable_hooks'] = FALSE;
+$config['subclass_prefix'] = 'EE_';
+$config['permitted_uri_chars'] = 'a-z 0-9~%.:_\\-';
+$config['enable_query_strings'] = FALSE;
+$config['directory_trigger'] = 'D';
+$config['controller_trigger'] = 'C';
+$config['function_trigger'] = 'M';
+$config['log_threshold'] = 0;
+$config['log_path'] = '';
+$config['log_date_format'] = 'Y-m-d H:i:s';
+$config['cache_path'] = '';
+$config['encryption_key'] = '';
+$config['global_xss_filtering'] = FALSE;
+$config['csrf_protection'] = FALSE;
+$config['compress_output'] = FALSE;
+$config['time_reference'] = 'local';
+$config['rewrite_short_tags'] = TRUE;
+$config['proxy_ips'] = '';
+
+/* 
+ * Universal database connection settings
+ -------------------------------------------------------------------*/
+$active_group = $env;
+$active_record = TRUE;
 
 $db[$active_group]['dbdriver'] = 'mysql';
 $db[$active_group]['pconnect'] = FALSE;
