7b524c64f72a94bab0c6cb87054cfb4e120d40b5
Brian Whitton <me@brianwhitton.com>
2013-11-19 18:05:32 -0500

    added compass config

---
+++ config.rb
@@ -0,0 +1,26 @@
+require 'compass-h5bp'
+require 'zurb-foundation'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "webroot/"
+css_dir = "webroot/assets/css"
+sass_dir = "webroot/assets/css/sass"
+images_dir = "webroot/assets/img"
+javascripts_dir = "webroot/assets/js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = environment == :production ? :compressed : :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
