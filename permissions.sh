ea8ce64ec3b1d9aceac2fd9dfdc0e2cddc1ff7b2
brian <me@brianwhitton.com>
2013-11-20 07:19:46 -0500

    working on set up script

--- permissions.sh
+++
@@ -1,9 +0,0 @@
-#! bin/sh
-echo 'Setting full permissions on EE Cache'
-echo
-chmod 777 webroot/system/expressionengine/cache
-echo 'Setting full permissions images folder and all sub folders'
-echo
-chmod -R 777 webroot/images
-
-
