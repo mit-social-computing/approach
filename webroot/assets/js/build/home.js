d62355ad1bb2d2ab865ec0a34a1dadaa8f681238
Brian Whitton <me@brianwhitton.com>
2014-05-29 13:43:03 -0400

    homepage with libgifjs

--- webroot/assets/js/build/home.js
+++ webroot/assets/js/build/home.js
@@ -1,3 +1,4 @@
+/*global SuperGif*/
 'use strict';
 
 var principles = document.getElementById('principles'),
@@ -14,41 +15,49 @@ var principles = document.getElementById('principles'),
         , 'storefront' : undefined
     },
     gifTemp = '/assets/img/gifs/{{gif}}.gif',
-    stage = document.getElementById('gifStage')
+    stage = document.getElementById('gifStage'),
+    principleText = document.getElementById('principles')
 
-Object.keys(gifs).forEach(function(key) {
-    var img = document.createElement('img')
-    img.src = gifTemp.replace('{{gif}}', key)
-    gifs[key] = img
-    if ( key === 'authentic' ) {
-        img.onload = function (e) {
-            stage.appendChild(this)
-            this.classList.add('show')
-        }
-    }
+function removeDisabled(gif) {
+    var href = '#' + gif.id,
+        anchor = document.querySelector('a[href="' + href +'"]')
+
+    anchor.classList.remove('disabled')
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    Array.prototype.forEach.call(principleText.children, function(el) {
+        el.gif = new SuperGif({
+            gif : document.getElementById(el.hash.slice(1)),
+            auto_play : 0,
+            c_h : 145
+        })
+        el.gif.load(removeDisabled)
+    })
 })
+// Object.keys(gifs).forEach(function(key) {
+//     var img = document.createElement('img')
+//     img.src = gifTemp.replace('{{gif}}', key)
+//     gifs[key] = img
+//     if ( key === 'authentic' ) {
+//         img.onload = function (e) {
+//             stage.appendChild(this)
+//             this.classList.add('show')
+//         }
+//     }
+// })
 
 principles.addEventListener('mouseenter', function(e) {
-    if ( e.target.nodeName === 'A' ) {
-        var gif
-        try {
-            gif = e.target.hash.substr(1)
-            stage.removeChild(stage.firstChild)
-        } catch(err) {}
-        finally {
-            stage.appendChild(gifs[gif])
-            gifs[gif].classList.add('show')
-        }
+    if ( e.target.nodeName === 'A' && !e.target.classList.contains('disabled') ) {
+        e.target.gif.move_to(0)
+        e.target.gif.get_canvas().classList.add('show')
+        e.target.gif.play()
     }
 }, true)
 
 principles.addEventListener('mouseleave', function(e) {
-    if ( e.target.nodeName === 'A' ) {
-        var gif = e.target.hash.substr(1)
-        gifs[gif].classList.remove('show')
-
-        try {
-            stage.removeChild(stage.firstChild)
-        } catch(err) {}
+    if ( e.target.nodeName === 'A' && !e.target.classList.contains('disabled') ) {
+        e.target.gif.get_canvas().classList.remove('show')
+        e.target.gif.pause()
     }
 }, true)
